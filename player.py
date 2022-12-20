f461e648ecaf9b16d76096431c4e65c7f1d1eb3e
Artem <achiraxov@yandex.ru>
2022-12-20 17:25:04 +0300

    Закончил работу над кодом для направления взгляда персонажа

--- player.py
+++ player.py
@@ -8,15 +8,36 @@ class player:
         self.game = game
         self.x = 1.5
         self.y = 1.5
-        self.angle = 0
+        self.kickguy_angle = 0
         self.player_speed = 0.004
         self.angle_speed = 0.002
         self.diagonal_speed = 0.004 * 0.70710678118
 
+    def angle(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        mouse_y /= 100
+        mouse_x /= 100
+        if mouse_y > self.y:
+            if mouse_x == self.x:
+                self.kickguy_angle = 1.570796325
+            elif mouse_x < self.x:
+                self.kickguy_angle = 3.14 + math.atan(((mouse_y) - self.y) / ((mouse_x) - self.x))
+            else:
+                self.kickguy_angle = math.atan(((mouse_y) - self.y) / ((mouse_x) - self.x))
+        else:
+            if mouse_x == self.x:
+                self.kickguy_angle = 4.712388975
+            elif mouse_x < self.x:
+                self.kickguy_angle = 3.14 + math.atan(((mouse_y) - self.y) / ((mouse_x) - self.x))
+            else:
+                self.kickguy_angle = math.atan(((mouse_y) - self.y) / ((mouse_x) - self.x))
+
+        self.kickguy_angle %= math.tau  # угол в модуле math считается в радианах
+
     def movement(self):
         counter = 0
-        sin_a = math.sin(self.angle)
-        cos_a = math.cos(self.angle)
+        sin_a = math.sin(self.kickguy_angle)
+        cos_a = math.cos(self.kickguy_angle)
         dx = 0
         dy = 0
         keys = pygame.key.get_pressed()
@@ -47,19 +68,16 @@ class player:
         self.x += dx
         self.y += dy
 
-        if keys[pygame.K_LEFT]:
-            self.angle -= self.angle_speed * self.game.delta_time
-        if keys[pygame.K_RIGHT]:
-            self.angle += self.angle_speed * self.game.delta_time
-        self.angle %= math.tau  # угол в модуле math считается в радианах
 
     def draw(self):
         pygame.draw.line(self.game.screen, 'blue', (self.x * 100, self.y * 100),
-                         (self.x * 100 + 720 * math.cos(self.angle), self.y * 100 + 720 * math.sin(self.angle)), 2)
+                         (self.x * 100 + 720 * math.cos(self.kickguy_angle),
+                          self.y * 100 + 720 * math.sin(self.kickguy_angle)), 2)
         pygame.draw.circle(self.game.screen, 'red', (self.x * 100, self.y * 100), 15)
 
     def update(self):
         self.movement()
+        self.angle()
 
     def cords(self):
         return self.x, self.y
