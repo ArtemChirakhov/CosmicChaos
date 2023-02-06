2505cf571ea80d3fbb64c8e495f96afb883d2bf9
Artem <achiraxov@yandex.ru>
2023-02-06 20:24:08 +0300

    Демо Стрельбы

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from blockmap import *
 from enemy import Enemy
 from player import *
 from raycasting import *
+from weapon import *
 
 
 class Game:
@@ -21,12 +22,14 @@ class Game:
         self.player1 = Player(self)
         self.map = Map(self)
         self.raycasting = RayCasting(self)
+        self.weapon = Weapon(self)
         # self.enemy = Enemy(self, 300, 300, 'red')
         self.enemy1 = Enemy(self, 400, 400, 'orange')
 
     def update(self):
         self.player1.update()
         self.raycasting.update()
+        self.weapon.update()
         self.enemy1.update()
         pygame.display.flip()
         self.delta_time = self.clock.tick(self.FPS)
