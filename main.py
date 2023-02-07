c50d87551aaaa5ad532e9fa6b2e2d078d8f47bdd
Vova <vovadelis@gmail.com>
2023-02-07 21:55:38 +0300

    Готовая версия начального меню

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
-
+import pygame_menu
 import pygame
 
 from raycasting import *
 from player import *
-from blockmap import*
+from blockmap import *
 
 
 class Game:
@@ -13,10 +13,14 @@ class Game:
 
     def __init__(self):
         pygame.init()
+        pygame.font.init()
         self.screen = pygame.display.set_mode(self.RES)
         self.clock = pygame.time.Clock()
         self.delta_time = 1
         self.new_game()
+        f1 = pygame.font.Font(None, 36)
+        self.text1 = f1.render('Hello Привет', True,
+                               (180, 0, 0))
 
     def new_game(self):
         self.kickguy = Player(self)
@@ -29,6 +33,7 @@ class Game:
         pygame.display.flip()
         self.delta_time = self.clock.tick(self.FPS)
         pygame.display.set_caption(f'{self.clock.get_fps() :.1f}')
+        self.screen.blit(self.text1, (10, 50))
 
     def draw(self):
         self.screen.fill('black')
@@ -47,7 +52,22 @@ class Game:
             self.update()
             self.draw()
 
+    def load_game(self):
+        pass
+
+    def menu(self):
+        font = pygame_menu.font.FONT_BEBAS
+        myimage = pygame_menu.baseimage.BaseImage(image_path="Data/cosmos1.jpg",
+                                                  drawing_mode=pygame_menu.baseimage.IMAGE_MODE_CENTER)
+        mytheme = pygame_menu.Theme(background_color=myimage, title_background_color=(0, 0, 0), title_font=font,
+                                    title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_SIMPLE, widget_font=font)
+        menu = pygame_menu.Menu('Cosmic Chaos', 1320, 720, theme=mytheme)
+        menu.add.button('New Game', self.run)
+        menu.add.button('Load game', self.load_game)
+        menu.add.button('Quit', pygame_menu.events.EXIT)
+        menu.mainloop(self.screen)
+
 
 if __name__ == '__main__':
     game = Game()
-    game.run()
+    game.menu()
